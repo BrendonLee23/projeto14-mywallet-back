5b007d70450d66a2b0d5a95489ff233477b63826
Brendo <brendow_prime@hotmail.com>
2023-07-07 03:15:15 -0400

    Conexão com o mongo aplicada

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoClient = new MongoClient("mongodb://localhost:27017/projeto-my-wallet");
+let db;
+
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message));
 
 
 app.get('/home', (req, res) => {
     res.send('SAlVE');
 });
 
-const PORT = 5000
+const PORT = 5000;
 app.listen(PORT, () => console.log('listening on port ${PORT}'));
