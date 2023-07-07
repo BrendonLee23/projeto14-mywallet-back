8df91409f82c4bd2f0c93ed0451d652dc8a0518b
Brendo <brendow_prime@hotmail.com>
2023-07-07 03:46:07 -0400

    dotenv configurado

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
-const mongoClient = new MongoClient("mongodb://localhost:27017/projeto-my-wallet");
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 mongoClient.connect()
@@ -18,5 +21,10 @@ app.get('/home', (req, res) => {
     res.send('SAlVE');
 });
 
+app.post('/home', (req, res) => {
+    res.send('SAlVE');
+});
+
 const PORT = 5000;
 app.listen(PORT, () => console.log('listening on port ${PORT}'));
+
