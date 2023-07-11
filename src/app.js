5de6abfb630a2d60d8eb5058368b530a5147c07d
Brendo <brendow_prime@hotmail.com>
2023-07-11 13:14:57 -0400

    app realocado para src

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import router from "./src/routes/index.routes.js";
+import router from "./routes/index.routes.js";
 
 // criação do App
 
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router)
-
 
 // Liga informações do servidor para escutar requisições
 
